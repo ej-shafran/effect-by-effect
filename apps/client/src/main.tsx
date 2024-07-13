64e4c752cb79779bc77bc0dd59730aaf6af42185
ejshafran <ejshafran@hilma.tech>
2024-07-13 20:39:25 +0300

    feat(client): add react

--- apps/client/src/main.tsx
+++ apps/client/src/main.tsx
@@ -1,1 +1,12 @@
-console.log("Hello from client!");
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { App } from "./App.tsx";
+import "./index.css";
+
+const root = document.getElementById("root");
+if (!root) throw new Error("failed to find #root in index.html");
+ReactDOM.createRoot(root).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
